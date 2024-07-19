2736349081c012076a2d36b59ec182b75a01170f
Olumide Olaoye <92790819+Olumide09@users.noreply.github.com>
2024-07-19 16:22:08 -0500

    Added text for confirm-email page, requires image for email icon

--- src/components/pages/ConfirmEmail.tsx
+++ src/components/pages/ConfirmEmail.tsx
@@ -1,19 +1,17 @@
 import { Button } from "components/atoms/Button";
 
-
-
-
 export function ConfirmEmail() {
-  function ConfirmEmail () {}
+  function ConfirmEmail() {}
 
   return (
     <div className="w-full h-screen flex flex-col justify-center items-center">
       <div className="flex flex-col items-center gap-4 text-center">
-
         <div className="w-16 h-16 bg-blue-100 flex items-center justify-center rounded-full">
-            <img src="#" alt="Email icon" className="w-8 h-8" />
+          <img src="#" alt="Email icon" className="w-8 h-8" />
         </div>
-        <h2 className=" text-2xl font-extrabold text-neutral-900">Confirm Email Address</h2>
+        <h2 className=" text-2xl font-extrabold text-neutral-900">
+          Confirm Email Address
+        </h2>
         <p className="text-neutral-600">
           We have just sent a verification link to your email.
         </p>
@@ -21,13 +19,12 @@ export function ConfirmEmail() {
         <p className="text-neutral600">
           Confirm your email address to continue on to the live demo!
         </p>
-        
+
         <Button
           text="Go to live Demo"
           className="text-white bg-blue-900 mt-4"
           onClick={ConfirmEmail}
         />
-      
       </div>
     </div>
   );
