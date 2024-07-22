3274b275f858d64fd3f98b04b0879e6c992ce2ab
Olumide Olaoye <92790819+Olumide09@users.noreply.github.com>
2024-07-22 16:06:51 -0500

    Making changes to the activity feed page

--- src/components/pages/ActivityLog.tsx
+++ src/components/pages/ActivityLog.tsx
@@ -1,3 +1,7 @@
+import React, { useState } from "react";
+import { Button } from "components/atoms/Button";
+
+
 export function ActivityLog() {
     return <h1>This is the Activity Log page</h1>;
   }
