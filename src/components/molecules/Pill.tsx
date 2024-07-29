2663e79c69f7bdf0e4d74d7fae045c4801f57be4
Tolu Thomas <tolu.thomas@protonmail.com>
2024-07-29 19:46:49 +0100

    Allow click and right icon on pill

--- src/components/molecules/Pill.tsx
+++ src/components/molecules/Pill.tsx
@@ -4,27 +4,32 @@ type TPillProps = {
   text: string;
 } & Partial<{
   icon: ReactNode;
+  rightIcon: ReactNode;
   containerClassName: string;
   textClass: string;
   textColor: string;
   secondText: string;
   secondTextClass: string;
   secondTextColor: string;
+  onClick: React.MouseEventHandler<HTMLDivElement>;
 }>;
 
 export function Pill({
   text,
   icon,
+  rightIcon,
   textClass = "font-semibold text-primary text-base",
   secondText,
   secondTextClass = "font-bold text-primary text-base",
   containerClassName = "row items-center rounded-[2.37rem] px-3 py-2 gap-1 bg-accent50",
+  onClick,
 }: TPillProps) {
   return (
-    <div className={containerClassName}>
+    <div className={containerClassName} onClick={onClick}>
       {icon ?? null}
       <h6 className={textClass}>{text}</h6>
       {secondText ? <h6 className={secondTextClass}>{secondText}</h6> : null}
+      {rightIcon ?? null}
     </div>
   );
 }
