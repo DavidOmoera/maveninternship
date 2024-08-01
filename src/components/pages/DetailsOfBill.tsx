41f9a0ba3fc1ba0603a275508dcea5a019b4709b
Tolu Thomas <tolu.thomas@protonmail.com>
2024-08-01 20:46:53 +0100

    Updated Bill Details UI to more closely match Figma design

--- src/components/pages/DetailsOfBill.tsx
+++ src/components/pages/DetailsOfBill.tsx
@@ -4,16 +4,20 @@ import { PageContainer } from "components/templates/PageContainer";
 import { Tabs } from "components/molecules/Tabs";
 import { Home2 } from "assets/Home2";
 import { Chat } from "assets/Chat";
+import tagIcon from "assets/tag.svg";
 import { Activity } from "assets/Activity";
 import { Clipboard } from "assets/Clipboard";
 import { TickSquare } from "assets/TickSquare";
 import { getTabSVGColor } from "utils/helpers";
 import {
   ABSTAINED_VOTERS,
+  colors,
   NO_VOTERS,
   TOTAL_VOTERS_COUNT,
   YES_VOTERS,
 } from "constants/common";
+import { Routes } from "types/routes";
+import classNames from "classnames";
 const AboutBill = lazy(() =>
   import("components/organisms/AboutBill").then((module) => ({
     default: module.AboutBill,
@@ -64,6 +68,23 @@ enum BILL_TAB {
   VOTING = "Voting",
 }
 
+const TAGS = ["Security", "Borders", "Immigration"];
+const DETAILS = [
+  { key: "Legislative Type", value: "Joint Resolution", link: "" },
+  { key: "Bill Status", value: "Passed", link: "" },
+  { key: "Current Status", value: "House Passage Report", link: "" },
+  { key: "Amendments", value: "2 Views", link: Routes.DetailsOfBill },
+];
+const VOTES_SUMMARY = [
+  { key: "Votes for", value: YES_VOTERS.length, iconColor: colors.success },
+  { key: "Votes against", value: NO_VOTERS.length, iconColor: colors.error },
+  {
+    key: "Abstained",
+    value: ABSTAINED_VOTERS.length,
+    iconColor: colors.orange,
+  },
+];
+
 const DetailsOfBill: React.FC = () => {
   const [activeTab, setActiveTab] = useState<BILL_TAB>(BILL_TAB.ABOUT);
 
@@ -113,80 +134,86 @@ const DetailsOfBill: React.FC = () => {
       <div className="flex-1 bg-gray-100 px-9 flex">
         <div className="w-full flex gap-4">
           {/* Bill Details Section */}
-          <div className="flex-1 p-9 bg-white rounded-xl mb-6 basis-1/4">
-            <h2
-              style={{
-                fontFamily: "Mulish",
-                fontSize: "32px",
-                fontWeight: 800,
-                lineHeight: "38.4px",
-                letterSpacing: "0.0025em",
-                textAlign: "left",
-                color: "#000000",
-              }}
-            >
+          <div className="flex-1 p-9 bg-white rounded-xl mb-6 basis-1/3">
+            <h2 className="text-neutral950 font-extrabold pb-6">
               Secure the Border Act of 2023
             </h2>
-            <p className="text-sm text-neutral600 mb-6">May 29, 2023</p>
-            <p className="text-sm text-neutral600 mb-6">
-              Security, Immigration, Borders
-            </p>
-            <div className="mb-6">
-              <h3 className="text-lg font-bold mb-2">Details</h3>
-              <p>
-                <strong>Legislative Type:</strong> Joint Resolution
-              </p>
-              <p>
-                <strong>Bill Status:</strong> Passed
-              </p>
-              <p>
-                <strong>Current Status:</strong> House Passage Report
-              </p>
-              <p>
-                <strong>Amendments:</strong>
-                <a href="#" className="text-primary">
-                  2 Views
-                </a>
-              </p>
+            <p className="text-sm text-neutral950">May 29, 2023</p>
+
+            <div className="row items-center gap-3 flex-wrap mt-2">
+              {TAGS.map((tag) => (
+                <div className="row gap-1 items-center">
+                  <img src={tagIcon} className="w-4 h-4" />
+                  <p className="text-xs text-neutral500">{tag}</p>
+                </div>
+              ))}
             </div>
-            <div className="bg-gray-50 p-6 rounded-lg">
-              <p className="text-4xl font-extrabold">{TOTAL_VOTERS_COUNT}</p>
-              <p className="text-lg text-neutral600">Votes for bill:</p>
-              <div className="flex items-center mt-2">
-                <span className="w-3 h-3 rounded-full bg-green-500 inline-block mr-2"></span>
-                <p className="text-green-600">Votes for: {YES_VOTERS.length}</p>
-              </div>
-              <div className="flex items-center mt-2">
-                <span className="w-3 h-3 rounded-full bg-red-500 inline-block mr-2"></span>
-                <p className="text-red-600">
-                  Votes against: {NO_VOTERS.length}
-                </p>
-              </div>
-              <div className="flex items-center mt-2">
-                <span className="w-3 h-3 rounded-full bg-yellow-500 inline-block mr-2"></span>
-                <p className="text-yellow-600">
-                  Abstained: {ABSTAINED_VOTERS.length}
-                </p>
+
+            <hr className="bg-neutral500 my-6" />
+
+            <div>
+              <h6 className="underline mb-3">Details</h6>
+              <div className="col gap-2">
+                {DETAILS.map((detail) => (
+                  <div className="row justify-between w-full">
+                    <p className="text-sm text-neutral500">{detail.key}</p>
+                    <h6
+                      className={classNames("text-sm text-neutral950", {
+                        "underline text-accent800 cursor-pointer":
+                          !!detail.link,
+                      })}
+                      onClick={() => {}}
+                    >
+                      {detail.value}
+                    </h6>
+                  </div>
+                ))}
               </div>
             </div>
-            <div className="mt-6">
-              <strong>Author:</strong>
-              <div className="flex items-center mt-2">
-                <img
-                  src={senatemat}
-                  alt="Author"
-                  className="w-10 h-10 rounded-full mr-3"
-                />
-                <div>
-                  <p className="font-bold">Sen. Mat Adams</p>
-                  <p className="text-neutral600">Senator</p>
+
+            <hr className="bg-neutral500 my-6" />
+
+            {/** Votes Summary */}
+            <div className="bg-gray-50 p-6 rounded-xl">
+              <p className="text-5xl font-extrabold">{TOTAL_VOTERS_COUNT}</p>
+              <h6 className="text-sm text-neutral400 mt-1">Votes for bill:</h6>
+              <div className="col gap-2 mt-5 mb-6">
+                {VOTES_SUMMARY.map((vote) => (
+                  <div
+                    key={vote.key}
+                    className="row w-full justify-between items-center"
+                  >
+                    <div className="row gap-2 items-center">
+                      <div
+                        className="h-3 w-3 rounded-full border-2"
+                        style={{ borderColor: vote.iconColor }}
+                      />
+                      <p className="text-sm text-neutral950">{vote.key}</p>
+                    </div>
+                    <h6 className="text-sm text-neutral950">{vote.value}</h6>
+                  </div>
+                ))}
+              </div>
+
+              <div>
+                <strong>Author:</strong>
+                <div className="flex items-center mt-3 py-2 px-3 bg-white">
+                  <img
+                    src={senatemat}
+                    alt="Author"
+                    className="w-10 h-10 rounded-full mr-3"
+                  />
+                  <div>
+                    <p className="font-bold">Sen. Mat Adams</p>
+                    <p className="text-neutral600">Senator</p>
+                  </div>
                 </div>
               </div>
             </div>
           </div>
 
           {/* Main Tabs */}
-          <div className="flex-1 gap-6 basis-3/4">
+          <div className="flex-1 gap-6 basis-2/3">
             <div className="flex-1 p-9 bg-white rounded-xl">
               {/* Navigation Bar */}
               <Tabs
