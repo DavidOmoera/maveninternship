f6295cf6285079218e173abb808bde962e30a421
DavidOmoera <115808365+DavidOmoera@users.noreply.github.com>
2024-08-03 01:28:19 +0000

    added side image design for login

--- src/components/templates/UnauthenticatedRoot.tsx
+++ src/components/templates/UnauthenticatedRoot.tsx
@@ -1,6 +1,7 @@
 import { useEffect } from "react";
 import { Outlet, useLocation, useNavigate } from "react-router-dom";
 import { Routes } from "types/routes";
+import sideImage  from "assets/side_image.svg";
 
 export function UnauthenticatedRoot() {
   const location = useLocation();
@@ -13,7 +14,13 @@ export function UnauthenticatedRoot() {
   return (
     <main className="md:flex md:flex-row h-full w-full">
       {/** On small devices, hide side image  */}
-      <aside className="hidden md:flex md:basis-[33.3%] bg-accent500" />
+      <aside className="hidden md:flex md:basis-[33.3%] bg-accent500">
+        <img
+          src={sideImage}
+          alt="Side decoration"
+          className="object-cover w-full h-full"
+        />
+      </aside>
 
       {/** Content */}
       <div className="md:basis-[66.7%]">
