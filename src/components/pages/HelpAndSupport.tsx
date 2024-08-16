2c850fdd6403e4c6df97a8bc1f323264076b1565
Grace Paul <graycepaul02@gmail.com>
2024-08-16 18:10:47 +0100

    Removed Contact card from help and support

--- src/components/pages/HelpAndSupport.tsx
+++ src/components/pages/HelpAndSupport.tsx
@@ -63,7 +63,7 @@ export function HelpAndSupport() {
   return (
     <PageContainer title="Help & Support">
       <div className="row justify-between bg-white mx-9 mt-6 rounded-xl p-9 lg:py-10 lg:px-16 xl:py-20 xl:px-32 gap-32">
-        <section className="col md:basis-[52%] md:max-w-[670px]">
+        <section className="col w-[700px]">
           <article className="col gap-1">
             <h1 className="text-black font-bold text-4xl">Get in Touch</h1>
             <p className="text-neutral500 text-lg">
@@ -138,22 +138,6 @@ export function HelpAndSupport() {
             className="mt-6"
           />
         </section>
-        <div className="hidden md:flex md:flex-col md:justify-start md:basis-[37%] max-w-[480px]">
-          <div className="bg-accent800 p-9 rounded-2xl gap-6">
-            <h1 className="font-bold text-4xl text-white">Contact Us Online</h1>
-            <div className="col gap-5 mt-6">
-              {CONTACT_DETAILS.map((contact, index) => (
-                <div
-                  key={contact.text + index}
-                  className="row gap-2 items-center"
-                >
-                  {contact.Icon}
-                  <p className="text-white text-lg">{contact.text}</p>
-                </div>
-              ))}
-            </div>
-          </div>
-        </div>
       </div>
     </PageContainer>
   );
