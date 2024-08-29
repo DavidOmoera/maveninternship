eedb6f870233fce3b63027fbc0c7db5fefaa84ca
Tolu Thomas <tolu.thomas@protonmail.com>
2024-08-29 22:23:45 +0100

    Display feedback to users

--- src/main.tsx
+++ src/main.tsx
@@ -5,11 +5,13 @@ import routes from "constants/routes";
 import { Provider } from "react-redux";
 import { store, persistor } from "store";
 import { PersistGate } from "redux-persist/integration/react";
+import { Toaster } from "react-hot-toast";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <Provider store={store}>
     <PersistGate loading={null} persistor={persistor}>
       <RouterProvider router={routes} />
+      <Toaster />
     </PersistGate>
   </Provider>
 );
