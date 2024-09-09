2135cb3dd09e09187c87a42ad504c088c42a0178
Tolu Thomas <tolu.thomas@protonmail.com>
2024-09-09 18:01:17 +0100

    Updated create feedback request

--- src/api/feedbackApi.ts
+++ src/api/feedbackApi.ts
@@ -2,15 +2,17 @@ import { AxiosResponse } from "axios";
 import { client } from "./client";
 import { endpoints } from "./endpoints";
 import {
-    THelpAndSupportForm,
-} from "types/common";;
+  TCreateFeedbackRequestBody,
+  TCreateFeedbackResponse,
+  THelpAndSupportForm,
+} from "types/common";
 
 export const createFeedback = (
-    params: THelpAndSupportForm
-  ): Promise<AxiosResponse<string>> =>
-    client.post(endpoints.feedback.getFeedback(), { params });
-  
-  export const getFeedback = (
-    params: THelpAndSupportForm
-  ): Promise<AxiosResponse<THelpAndSupportForm[]>> =>
-    client.get(endpoints.feedback.getFeedback(), { params });
+  requestBody: TCreateFeedbackRequestBody
+): Promise<AxiosResponse<TCreateFeedbackResponse>> =>
+  client.post(endpoints.feedback.getFeedback(), requestBody);
+
+export const getFeedback = (
+  params: THelpAndSupportForm
+): Promise<AxiosResponse<THelpAndSupportForm[]>> =>
+  client.get(endpoints.feedback.getFeedback(), { params });
