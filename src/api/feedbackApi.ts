aea01a0bb8ed97d554be2fd65d1bb84f068f1d8b
Tolu Thomas <tolu.thomas@protonmail.com>
2024-09-09 18:01:43 +0100

    Removed unused getFeedback

--- src/api/feedbackApi.ts
+++ src/api/feedbackApi.ts
@@ -4,15 +4,9 @@ import { endpoints } from "./endpoints";
 import {
   TCreateFeedbackRequestBody,
   TCreateFeedbackResponse,
-  THelpAndSupportForm,
 } from "types/common";
 
 export const createFeedback = (
   requestBody: TCreateFeedbackRequestBody
 ): Promise<AxiosResponse<TCreateFeedbackResponse>> =>
   client.post(endpoints.feedback.getFeedback(), requestBody);
-
-export const getFeedback = (
-  params: THelpAndSupportForm
-): Promise<AxiosResponse<THelpAndSupportForm[]>> =>
-  client.get(endpoints.feedback.getFeedback(), { params });
