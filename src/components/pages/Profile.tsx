d2c7d90ab27de5e87b5839f2d6795e640bc5ec61
Grace Paul <graycepaul02@gmail.com>
2024-09-13 20:33:09 +0100

    no change

--- src/components/pages/Profile.tsx
+++ src/components/pages/Profile.tsx
@@ -314,6 +314,7 @@ export function Profile() {
   function onClickManagePaymentMethod() {
     setShowManagePaymentMethodForm(true);
   }
+
   function onClickEditOrgDetails() {
     setShowOrganizationDetailsForm(true);
   }
